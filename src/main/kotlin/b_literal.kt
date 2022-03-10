1974c44441d93e0bc8df8d9eb2b7ceadc9c67e78
RPL-01 <mhddicky171001@gmail.com>
2022-03-10 14:45:19 +0700

    Program Pertama Kotlin

---
+++ src/main/kotlin/b_literal.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val intLiteral = 5
+    val doubleLiteral = .02
+    val stringLiteral = "Hello"
+    val charLiteral = '1'
+    val boolLiteral = true
+    println("intLiteral = " + intLiteral)
+    println("doubleLiteral = " + doubleLiteral)
+    println("stringLiteral = " + stringLiteral)
+    println("charLiteral = " + charLiteral)
+    println("boolLiteral = " + boolLiteral)
+}
