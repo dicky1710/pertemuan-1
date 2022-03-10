ac1ab5435037637584aaed5f6eb0a68bbf3a0cf6
RPL-01 <mhddicky171001@gmail.com>
2022-03-10 15:33:49 +0700

    var dan var vs val

---
+++ src/main/kotlin/VarVsVal.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    // var (nilainya dapat berubah sepanjang program dieksekusi)
+    var varVariabel: Int
+    varVariabel = 56 //assigning first value to varVariabel
+    println("Pemberian nilai pertama pada varVariabel = "+varVariabel)
+
+    varVariabel = 78 //assigning second value to varVariabel
+    println("Pemberian nilai kedua pada varVariabel = "+varVariabel)
+    // val (nilainya tidak bisa berubah dan hanya bisa diberikan nilai sekali saja ketika program dieksekusi)
+    val phi = 3.14
+}
